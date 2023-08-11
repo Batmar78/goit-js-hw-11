b39271a81d51711604bc2f241f3797f1fd853da5
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-08-11 22:19:32 +0300

    js-hw-11_v_02

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-console.dir(axios);
-console.dir(Notify );
+// console.dir(axios);
+// console.dir(Notify);
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "38704294-b9169c0a05cb876a56f757da2";
+
+const form = document.querySelector('.search-form');
+
+
+form.addEventListener('submit', handlerForm);
+
+function handlerForm(evt) {
+    evt.preventDefault();
+    const searchImage = evt.currentTarget.searchQuery.value;
+    // getImages(searchImage);
+}
+
+async function getImages(searchImage) {
+    try {
+        const resp = await axios.get(`${BASE_URL}`,
+            {
+            params: {
+                key: API_KEY,
+                q: searchImage,
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: true,
+            }
+            }
+        );
+        const data = resp.json();
+        return data;
+    } catch (error) {
+        console.log("Помилка TryCatch")
+    }       
+    
+};
+
+    
+
+ 
+getImages()
+    .then(data => console.log(data))
+    .catch((e) =>
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        
+        );
+
+// console.log(form);

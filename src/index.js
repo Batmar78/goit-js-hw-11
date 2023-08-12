f80e69936d9d40def2e22bac39e9a304f7054d4f
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-08-12 23:48:34 +0300

    js-hw-11_v_03

--- src/index.js
+++ src/index.js
@@ -8,20 +8,32 @@ const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "38704294-b9169c0a05cb876a56f757da2";
 
 const form = document.querySelector('.search-form');
-
+const galery = document.querySelector('.gallery');
 
 form.addEventListener('submit', handlerForm);
 
 function handlerForm(evt) {
     evt.preventDefault();
     const searchImage = evt.currentTarget.searchQuery.value;
-    // getImages(searchImage);
+    getImages(searchImage)
+        .then(resp => {
+            const cards = resp.data.hits
+            Notify.success(`Hooray! We found ${resp.data.totalHits} images.`)
+            console.log(cards)
+            galery.insertAdjacentHTML('beforeend', createMurcup(cards)) 
+        })
+        .catch((e) =>
+           
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        
+        );
+
 }
 
 async function getImages(searchImage) {
-    try {
-        const resp = await axios.get(`${BASE_URL}`,
-            {
+  
+    const resp = await axios.get(BASE_URL,
+        {
             params: {
                 key: API_KEY,
                 q: searchImage,
@@ -29,24 +41,40 @@ async function getImages(searchImage) {
                 orientation: "horizontal",
                 safesearch: true,
             }
-            }
-        );
-        const data = resp.json();
-        return data;
-    } catch (error) {
-        console.log("Помилка TryCatch")
-    }       
+        }
+            
+    );
+    return resp;
+    
+          
     
 };
 
-    
+function createMurcup(arr) {
+    return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>`).join('');
+}
 
- 
-getImages()
-    .then(data => console.log(data))
-    .catch((e) =>
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+// getImages()
+//     .then(resp => console.log(resp))
+//     .catch((e) =>
+//         Notify.failure("Sorry, there are no images matching your search query. Please try again.")
         
-        );
+//         );
 
 // console.log(form);
